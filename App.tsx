68c6c0e13280fb09eef87b8a561f6d24f1f8f336
Bogdan Selenginskiy <44996583+bseleng@users.noreply.github.com>
2022-06-01 18:34:49 +0300

    SEC 15 | feat:  add context creator without types

--- App.tsx
+++ App.tsx
@@ -4,24 +4,27 @@ import { createStackNavigator } from "@react-navigation/stack";
 import AuthFlowNavigator from "./src/Navigators/AuthFlowNavigator";
 import MainFlowNavigator from "./src/Navigators/MainFlowNavigator";
 import Routes from "./src/Navigators/routes";
+import { Provider as AuthProvider } from "./src/context/AuthContext";
 
 const Stack = createStackNavigator();
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          animationEnabled: false,
-        }}
-      >
-        <Stack.Screen
-          name={Routes.Guest}
-          component={AuthFlowNavigator}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name={Routes.User} component={MainFlowNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            animationEnabled: false,
+          }}
+        >
+          <Stack.Screen
+            name={Routes.Guest}
+            component={AuthFlowNavigator}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name={Routes.User} component={MainFlowNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthProvider>
   );
 };
 
---
+++ src/context/AuthContext.ts
@@ -0,0 +1,14 @@
+import createDataContext from "./CreateDataContext";
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export const { Provider, Context } = createDataContext(
+  authReducer,
+  {},
+  { isSignedIn: false }
+);
