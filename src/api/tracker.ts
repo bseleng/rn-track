fd1c71cf5e8648bbd30911d1279096df6c6e40cf
Bogdan Selenginskiy <44996583+bseleng@users.noreply.github.com>
2022-06-16 22:09:40 +0300

    SEC 15 | feat: add navigation state and get parent types

--- src/api/tracker.ts
+++ src/api/tracker.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://wet-pandas-start-95-182-91-13.loca.lt",
+  baseURL: "https://khaki-waves-guess-95-182-91-13.loca.lt",
 });
--- src/types/thirdParty/navigation.ts
+++ src/types/thirdParty/navigation.ts
@@ -1,11 +1,22 @@
 type navListenerCallback = (navigationEvent: Event) => void;
 
+type NavigationState = {
+  index: number;
+  key: string;
+  routeNames?: string[];
+  routes: { key: string; name: string; params: unknown }[];
+  stale: boolean;
+  type: string;
+};
+
 type Navigation = {
   navigate: (route: string) => void;
   addListener: (
     navigationEvent: string,
     navListenerCallback: navListenerCallback
   ) => void;
+  getParent: (id?: string) => Navigation;
+  getState: () => NavigationState;
 };
 
 export default Navigation;
